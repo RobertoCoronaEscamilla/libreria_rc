f2de5c9ad6c93f416a2a5990443e11b86f6e8538
Roberto Corona <rober.corona@gmail.com>
2023-01-26 02:47:14 +0100

    refactorizaciones2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
   name = 'funciones',         # How you named your package folder (MyLib)
-  packages = ['funciones'],   # Chose the same as "name"
+  packages = ['src.funciones'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Libreria de mates',   # Give a short description about your library
